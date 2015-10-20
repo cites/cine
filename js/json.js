6b0aca4e9b480f60aaa4e526f49d4ddab6f0b16c
cites <pintososcar@gmail.com>
2015-10-19 22:48:15 -0300

    algunos colores

--- js/json.js
+++ js/json.js
@@ -7,9 +7,9 @@ function omdb(url){
 }
 
 function cambiacolor_over(celda){ 
-	celda.style.backgroundColor="#00006B"
+	celda.style.backgroundColor="#4DB8B8"
 	 }
 
 function cambiacolor_out(celda){
- celda.style.backgroundColor="#ffffff"
+ celda.style.backgroundColor="#293D3D"
   } 
