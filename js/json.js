4510e0d1135589ea6820c42f9e1a25d5abfec51e
root <root@oscar.org>
2015-10-20 23:30:42 -0300

    probando stilos

--- js/json.js
+++ js/json.js
@@ -6,10 +6,3 @@ function omdb(url){
  });
 }
 
-function cambiacolor_over(celda){ 
-	celda.style.backgroundColor="#4DB8B8"
-	 }
-
-function cambiacolor_out(celda){
- celda.style.backgroundColor="#293D3D"
-  } 
---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function cambiacolor_over(celda){ 
+	celda.style.backgroundColor="#4DB8B8"
+	 }
+
+function cambiacolor_out(celda){
+ celda.style.backgroundColor="#293D3D"
+  } 
